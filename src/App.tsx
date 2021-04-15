11e13c6cc2504a7b78cb0c8d733186e321f36fc4
Noah <ncharris93@gmail.com>
2021-04-15 18:14:44 -0400

    add react-router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { UserContainer } from './containers/UserContainer'
+import { Route, Router, Switch, useHistory } from 'react-router-dom'
 
 function App() {
+  const history = useHistory()
   return (
     <div className="App">
       <header className="App-header">
-        <UserContainer />
+        <Router history={history}>
+          <Switch>
+            <Route path="/">
+              <UserContainer />
+            </Route>
+            <Route path="/user-form">{/* <UserForm /> */}</Route>
+            <Route path="/user-view">{/* <UserView /> */}</Route>
+          </Switch>
+        </Router>
       </header>
     </div>
   )
